edb3db0bc75b8b2e8755f5478ad740e4102df73c
KadiataB <kadiatab79@gmail.com>
2023-03-07 18:21:09 +0000

    deplacement des éléments sélectionnés de la droite vers la gauche

--- script.js
+++ script.js
@@ -14,11 +14,56 @@ generateur()
 //survoler un element et le selectionner
 let survol=document.querySelectorAll('p')
 for(let i=0; i<survol.length; i++){
-    survol[i].addEventListener("mouseenter", () => {
-    survol[i].className = "active";
+    survol[i].addEventListener("mouseover", () => {
+     if (survol[i].classList.contains("active")){
+        survol[i].classList.remove("active")
+    }
+    else{
+         survol[i].classList.add("active")
+     }
     })
-    survol[i].addEventListener("mouseleave", () => {
-    survol[i].className = "p";
-    })
+    
 }
+
+
+//récupération du bonton Khadijatou Ba
+let btn = document.querySelector(".btn");
+
+//activation ou désactivation du bouton
+btn.disabled = false;
+
+
+let rightPart = document.querySelector('.rightPart')
+
+btn.addEventListener("change", stateHandle());
+//permet de vérifier si la div droite contient des éléments
+function stateHandle() {
+  if (rightPart.hasChildNodes()) {
+    btn.disabled = false;
+  } else {
+    btn.disabled = true;
+  }
+}
+
+//function supprimer et ajouter 
+
+btn.addEventListener("click", () => {
+  let leftPart = document.querySelector('.leftPart')
+  let items = document.querySelectorAll('.rightPart p')
+
+  for (const item of items) {
+    if (item.className == "active") {
+      let m = rightPart.removeChild(item)
+      leftPart.append(m)
+      m.classList.remove("active")
+    }
+  }
+  
+})
+
+
+
+
+
+
 
